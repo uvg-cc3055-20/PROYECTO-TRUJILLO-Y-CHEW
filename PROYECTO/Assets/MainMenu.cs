a2c8def4b07305586588d8f4024a75164650977b
Carlos Chew <33320933+CarlosChew@users.noreply.github.com>
2018-02-26 17:27:37 -0600

    New features

--- PROYECTO/Assets/MainMenu.cs
+++ PROYECTO/Assets/MainMenu.cs
@@ -7,6 +7,13 @@ public class MainMenu : MonoBehaviour {
 
 	public void PlayGame(){
 
-		SceneManager.LoadScene(SceneManager.GetActiveScene ().buildIndex + 1);
+		SceneManager.LoadScene(2);
 	}
+
+	public void QuitGame(){
+
+		Debug.Log ("Quit");
+		Application.Quit ();
+	}
+
 }
