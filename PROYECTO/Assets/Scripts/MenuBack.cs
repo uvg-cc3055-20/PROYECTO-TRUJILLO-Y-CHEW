0d47f169686e535ba349ce17b661ae207172e6bc
tru17189 <32879914+tru17189@users.noreply.github.com>
2018-02-26 21:35:22 -0600

    12

---
+++ PROYECTO/Assets/Scripts/MenuBack.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuBack : MonoBehaviour {
+
+	public void bottonPlay(){
+	SceneManager.LoadScene("Menu", LoadSceneMode.Single);
+	}
+}
